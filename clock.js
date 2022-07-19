35023bce77cf49428bf12d51d776c986cd97e44b
JBhaha <69484637+JBhaha@users.noreply.github.com>
2022-07-19 18:14:35 +0200

    added checkTime function

--- clock.js
+++ clock.js
@@ -4,7 +4,16 @@ function clock() {
   let minute = date.getMinutes();
   let second = date.getSeconds();
 
+  hour = checkTime(hour);
+  minute = checkTime(minute);
+  second = checkTime(second);
+
   document.getElementById('clock').innerHTML =
     hour + ':' + minute + ':' + second;
   setTimeout(clock, 1000);
+}
+
+function checkTime(t) {
+  if (t < 10) return '0' + t;
+  else return t;
 }
